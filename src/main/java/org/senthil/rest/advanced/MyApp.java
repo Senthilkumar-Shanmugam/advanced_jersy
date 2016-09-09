903956c42def470f90afece6379144429b331ed0
tribles <sss.senthil@gmail.com>
2016-09-09 12:35:20 +0530

    adding jersy without web xml

---
+++ src/main/java/org/senthil/rest/advanced/MyApp.java
@@ -0,0 +1,12 @@
+package org.senthil.rest.advanced;
+
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("webapi")
+public class MyApp extends Application{
+
+
+}
