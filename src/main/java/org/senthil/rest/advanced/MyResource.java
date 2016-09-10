203bb7d3bc98f19132d127122e667201d07b6bfe
tribles <sss.senthil@gmail.com>
2016-09-10 18:44:23 +0530

    changes in master

--- src/main/java/org/senthil/rest/advanced/MyResource.java
+++ src/main/java/org/senthil/rest/advanced/MyResource.java
@@ -21,6 +21,6 @@ public class MyResource {
 	@GET
 	@Path("/hello")
 	public String sayHello(){
-		return "says hello"+"testing conflicts in branch";
+		return "says hello"+"testing conflicts this is from master";
 	}
 }
